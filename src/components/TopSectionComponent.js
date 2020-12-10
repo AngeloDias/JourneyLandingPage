d625af527d8d9ce6f105d18122329ce19a33889d
angelo.santana <angelossdias@gmail.com>
2020-12-10 10:26:23 -0300

    feat: inserting elements to component

--- src/components/TopSectionComponent.js
+++ src/components/TopSectionComponent.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import walkingBackground from '../images/walking-in-the-forest.jpg';
 
 export default function TopSectionComponent() {
     return (
-        <div className="iris">
-            {/* <img src={walkingBackground} className="responsive-img card transparent z-depth-5" alt="top" /> 
-            Reference: https://stackoverflow.com/questions/30113116/overlaying-an-image-with-text-in-materialize-css */}
+        <div className="iris valign-wrapper">
             <div className="card transparent z-depth-5"/>
-            <p className="grey-text text-darken-1">Content first part</p>
-            <p className="grey-text text-darken-1">Content second part</p>
+
+            <div className="center-align orange-text">
+                <div>
+                    <h1 className="text-darken-1">A Jornada</h1>
+                    <h5 className="text-darken-1">O ponto de partida para você se tornar gente de verdade</h5>
+                </div>
+
+                <div>
+                    <h2 className="flow-text"><strong>Dias 12, 13 e 14 de dezembro, às 21h, gratuitamente no Instagram <a href="http://instagram.com/rebecca.athayde">@rebecca.athayde</a></strong></h2>
+                </div>
+
+                <div className="divider-separator">
+                    <span className="elementor-separator"></span>
+                </div>
+
+                <div>
+                    <p className="flow-text">INSCREVA-SE GRÁTIS E RECEBA O MATERIAL EXCLUSIVO DAS AULAS</p>
+                </div>
+
+            </div>
         </div>
     )
 }
