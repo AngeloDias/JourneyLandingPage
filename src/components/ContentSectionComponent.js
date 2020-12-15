ef628bacb2f4bf79b3b2db7449f5eb4d5dd93526
angelo.santana <angelossdias@gmail.com>
2020-12-15 11:37:12 -0300

    feat: placing all cards in same responsive row

--- src/components/ContentSectionComponent.js
+++ src/components/ContentSectionComponent.js
@@ -6,49 +6,50 @@ import aula03 from '../images/aula03.jpg';
 export default function ContentSectionComponent() {
     return (
         <div>
-            <div className="row">
-                <div className="col s12 m6">
-                    <div className="card">
-                        <div className="card-image">
-                            <img src={aula01} className="responsive-img small-image-in-card" alt="card-aula-01" />
-                            <span className="card-title">Aula 01 - 11/12</span>
-                        </div>
+            <div>
 
-                        <div className="card-content">
-                            <p>Retirando as máscaras e reconhecendo sua imaturidade.</p>
+                <div className="row">
+                    <div className="col s12 m4">
+                        <div className="card">
+                            <div className="card-image">
+                                <img src={aula01} className="responsive-img small-image-in-card" alt="card-aula-01" />
+                                <span className="card-title">Aula 01 - 11/12</span>
+                            </div>
+
+                            <div className="card-content">
+                                <p>Retirando as máscaras e reconhecendo sua imaturidade.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                
+                    <div className="col s12 m4">
+                        <div className="card">
+                            <div className="card-image">
+                                <img src={aula02} className="responsive-img small-image-in-card" alt="card-aula-02" />
+                                <span className="card-title">Aula 02 - 12/12</span>
+                            </div>
 
-            <div className="row">
-                <div className="col s12 m6">
-                    <div className="card">
-                        <div className="card-image">
-                            <img src={aula02} className="responsive-img small-image-in-card" alt="card-aula-02" />
-                            <span className="card-title">Aula 02 - 12/12</span>
-                        </div>
-
-                        <div className="card-content">
-                            <p>Trilhando um caminho de crescimento na personalidade.</p>
+                            <div className="card-content">
+                                <p>Trilhando um caminho de crescimento na personalidade.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="row">
-                <div className="col s12 m6">
-                    <div className="card">
-                        <div className="card-image">
-                            <img src={aula03} className="responsive-img small-image-in-card" alt="card-aula-03" />
-                            <span className="card-title">Aula 03 - 13/12</span>
-                        </div>
+                    <div className="col s12 m4">
+                        <div className="card">
+                            <div className="card-image">
+                                <img src={aula03} className="responsive-img small-image-in-card" alt="card-aula-03" />
+                                <span className="card-title">Aula 03 - 13/12</span>
+                            </div>
 
-                        <div className="card-content">
-                            <p>Direcionando a sua vida ao que é belo, bom e verdadeiro.</p>
+                            <div className="card-content">
+                                <p>Direcionando a sua vida ao que é belo, bom e verdadeiro.</p>
+                            </div>
                         </div>
                     </div>
+
                 </div>
+
             </div>
 
             <div className="row"></div>
--- src/components/TopSectionComponent.js
+++ src/components/TopSectionComponent.js
@@ -5,50 +5,60 @@ export default function TopSectionComponent() {
         <div className="iris valign-wrapper responsive-img">
             <div className="card transparent z-depth-5" />
 
-            <div className="center-align white-text">
+            <div className="center-align white-text wrapper-content">
                 <div>
                     <h1 className="text-darken-1">A Jornada</h1>
                     <h4 className="text-darken-1">O ponto de partida para você se tornar gente de verdade</h4>
                 </div>
 
-                <div>
-                    <h6 className="flow-text"><strong>Dias 11, 12 e 13 de dezembro, às 21 h, gratuitamente no Instagram <a href="http://instagram.com/rebecca.athayde">@rebecca.athayde</a></strong></h6>
-                </div>
-
-                <div className="dividers-container responsive-img">
-                    <div className="divider-separator" />
-                    <div className="divider-separator" />
-                    <div className="divider-separator" />
-                    <div className="divider-separator" />
-                    <div className="divider-separator" />
-                    <div className="divider-separator" />
-                    <div className="divider-separator" />
-                    <div className="divider-separator" />
-                    <div className="divider-separator" />
-                </div>
-
-                <div>
-                    <p className="flow-text">INSCREVA - SE GRÁTIS E RECEBA O MATERIAL EXCLUSIVO DAS AULAS</p>
-                </div>
-
                 <div className="row">
-                    <form method="post" className="col s12">
+                    <div className="col s12 m6"></div>
+                    
+                    <div className="col s12 m6">
+
+                        <div>
+                            <h6 className="flow-text"><strong>Dias 11, 12 e 13 de dezembro, às 21 h, gratuitamente no Instagram <a href="http://instagram.com/rebecca.athayde">@rebecca.athayde</a></strong></h6>
+                        </div>
+
+                        <div className="dividers-container responsive-img">
+                            <div className="divider-separator" />
+                            <div className="divider-separator" />
+                            <div className="divider-separator" />
+                            <div className="divider-separator" />
+                            <div className="divider-separator" />
+                            <div className="divider-separator" />
+                            <div className="divider-separator" />
+                            <div className="divider-separator" />
+                            <div className="divider-separator" />
+                        </div>
+
                         <div className="row">
-                            <div className="input-field col s12">
-                                <input id="first_name" type="text" className="validate"/>
-                                <label htmlFor="first_name">Seu primeiro nome</label>
+                            <div>
+                                <p className="flow-text p-subscribe-asking"><strong>INSCREVA-SE GRÁTIS E RECEBA O MATERIAL EXCLUSIVO DAS AULAS</strong></p>
                             </div>
+
+                            <form method="post" className="col s12 m12">
+                                <div className="row">
+                                    <div className="input-field col s12 input-to-blank">
+                                        <input id="first_name" type="text" className="validate"/>
+                                        <label htmlFor="first_name">Seu primeiro nome</label>
+                                    </div>
+                                </div>
+
+                                <div className="row">
+                                    <div className="input-field col s12 input-to-blank">
+                                        <input id="email" type="email" className="validate"/>
+                                        <label htmlFor="email">Seu melhor email</label>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <button className="btn waves-effect waves-orange orange btn-to-width" type="submit" name="action">Iniciar a minha jornada</button>
+                                </div>
+                            </form>
                         </div>
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <input id="email" type="email" className="validate"/>
-                                <label htmlFor="email">Seu melhor email</label>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <button class="btn waves-effect waves-light orange" type="submit" name="action">Iniciar a minha jornada</button>
-                        </div>
-                    </form>
+
+                    </div>
+
                 </div>
 
             </div>
