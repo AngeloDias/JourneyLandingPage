c0196ac36005aab2d6ca94b62b7cd8923a62bcc8
angelodias <angelossdias@gmail.com>
2020-12-16 19:52:00 -0300

    feat: removing some comments

--- src/components/EntirePageComponent.js
+++ src/components/EntirePageComponent.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 import TopSectionComponent from './TopSectionComponent';
 import ContentSectionComponent from './ContentSectionComponent';
 
-// Portfolio
-// http://labs.qnimate.com/portfolio-materialize/
-
-// Materialize CSS tutorial
-// https://scotch.io/tutorials/make-material-design-websites-with-the-materialize-css-framework
-
 export default class EntirePageComponent extends Component {
     render() {
         return(
