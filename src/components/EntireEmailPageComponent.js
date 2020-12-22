40b9920a47e657fbafb24e6b62bbd4d179d4d2b3
angelo.santana <angelossdias@gmail.com>
2020-12-22 10:45:46 -0300

    feat: removing references to symfony-react tutorials

--- src/components/EntireEmailPageComponent.js
+++ src/components/EntireEmailPageComponent.js
@@ -4,11 +4,6 @@ import TopEmailSectionComponent from './TopEmailSectionComponent';
 /* Reference to create application using Docker */
 // https://github.com/malithmcr/react-php-contact-form
 
-/* References to build single page app with Symfony and React */
-// https://www.twilio.com/blog/building-a-single-page-application-with-symfony-php-and-react
-// https://symfony.com/doc/current/frontend/encore/reactjs.html
-/*  */
-
 /* Reference to create form to create form using React */
 // https://blog.mailtrap.io/react-contact-form/
 
